adb5f79a6e2b9a8a0c7166592969786352d82dcf
Eivind Fonn <evfonn@gmail.com>
2019-04-12 17:31:43 +0200

    Transforms: add Updim

--- src/Transforms.jl
+++ src/Transforms.jl
@@ -1,9 +1,11 @@
 module Transforms
 
+import Base.Iterators: flatten
+import Base: @_inline_meta
 using StaticArrays
 
 export Transform
-export Empty, Shift
+export Empty, Shift, Updim
 
 
 """
@@ -14,7 +16,7 @@ with elements of type R.
 """
 abstract type Transform{From, To, R<:Real} end
 
-@inline fromdims(::Type{<:Transform{F}}) where {F} = F
+@inline fromdims(::Type{<:Transform{F}}) where F = F
 @inline todims(::Type{<:Transform{_F, T}}) where {_F, T} = T
 @inline eltype(::Type{<:Transform{_F, _T, R}}) where {_F, _T, R} = R
 
@@ -50,6 +52,61 @@ end
 
 @inline (self::Shift)(point) = point + self.offset
 @inline (self::Shift)(point, grad) = (point + self.offset, grad)
+
+
+"""
+    Updim{I, N}(value)
+
+Create a transformation increasing the dimension of a space by one, by
+inserting the element `value` at index `I`. The final space has
+dimension `N`.
+"""
+struct Updim{Ins, From, To, R} <: Transform{From, To, R}
+    value :: R
+
+    @inline function Updim{Ins, To}(value) where {Ins, To}
+        @assert 1 <= Ins <= To
+        @assert 1 <= To <= 3
+        new{Ins, To-1, To, typeof(value)}(value)
+    end
+end
+
+@generated function (self::Updim{Ins})(point) where Ins
+    elements = [:(point[$i]) for i in 1:fromdims(self)]
+    insert!(elements, Ins, :(self.value))
+    quote
+        @_inline_meta
+        @SVector [$(elements...)]
+    end
+end
+
+@generated function (self::Updim{Ins})(point, grad) where Ins
+    from = fromdims(self)
+    to = todims(self)
+    R = eltype(self)
+
+    src_cols = [[:(grad[$i,$j]) for i in 1:from] for j in 1:from]
+    for col in src_cols
+        insert!(col, Ins, :(zero($R)))
+    end
+
+    if to == 1
+        new_col = [:(one($R))]
+    elseif to == 2
+        ((a, b),) = src_cols
+        new_col = [b, :(-$a)]
+    elseif to == 3
+        ((a, c, e), (b, d, f)) = src_cols
+        new_col = [:($c*$f - $e*$d), :($e*$b - $a*$f), :($a*$d - $c*$b)]
+    end
+
+    elements = flatten((src_cols..., new_col))
+    quote
+        @_inline_meta
+        newgrad = SMatrix{$to,$to}($(elements...))
+        (self(point), newgrad)
+    end
+end
 
 
 end # module
--- test/Transforms.jl
+++ test/Transforms.jl
@@ -14,7 +14,7 @@
         initial = @SVector rand(4)
         grad = SMatrix{4,4,Float64}(I)
         @bench begin
-            trf = Empty(4)
+            trf = Empty{4, Float64}()
             trf($initial)
             trf($initial, $grad)
         end
@@ -41,6 +41,63 @@ end
             trf = Shift(@SVector rand(4))
             trf($initial)
             trf($initial, $grad)
+        end
+    end
+end
+
+
+@testset "Updim" begin
+    Random.seed!(201904121710)
+
+    # Dimension 2 -> 3
+    initial = @SVector rand(2)
+    igrad = SMatrix{2,2}(1.0, 2.0, 3.0, 4.0)
+
+    trf = Updim{1,3}(rand(Float64))
+    (point, grad) = trf(initial, igrad)
+    @test point == [trf.value, initial...]
+    @test grad == [0 0 -2; 1 3 0; 2 4 0]
+
+    trf = Updim{2,3}(rand(Float64))
+    (point, grad) = trf(initial, igrad)
+    @test point == [initial[1], trf.value, initial[2]]
+    @test grad == [1 3 0; 0 0 2; 2 4 0]
+
+    trf = Updim{3,3}(rand(Float64))
+    (point, grad) = trf(initial, igrad)
+    @test point == [initial..., trf.value]
+    @test grad == [1 3 0; 2 4 0; 0 0 -2]
+
+    # Dimension 1 -> 2
+    initial = @SVector rand(1)
+    igrad = SMatrix{1,1}(3.0)
+
+    trf = Updim{1,2}(rand(Float64))
+    (point, grad) = trf(initial, igrad)
+    @test point == [trf.value, initial...]
+    @test grad == [0 3; 3 0]
+
+    trf = Updim{2,2}(rand(Float64))
+    (point, grad) = trf(initial, igrad)
+    @test point == [initial..., trf.value]
+    @test grad == [3 0; 0 -3]
+
+    # Dimension 0 -> 1
+    initial = SVector{0,Float64}()
+    igrad = SMatrix{0,0,Float64}()
+
+    trf = Updim{1,1}(rand(Float64))
+    (point, grad) = trf(initial, igrad)
+    @test point == [trf.value]
+    @test grad == ones(1,1)
+
+    @noallocs begin
+        initial = @SVector rand(2)
+        igrad = SMatrix{2,2}(1.0, 2.0, 3.0, 4.0)
+        @bench begin
+            trf = Updim{2,3}(rand(Float64))
+            trf($initial)
+            trf($initial, $igrad)
         end
     end
 end
