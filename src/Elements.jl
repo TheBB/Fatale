e24a6b591fea3c11432c3de5aff14de73a2ef2ad
Eivind Fonn <evfonn@gmail.com>
2019-06-13 16:44:37 +0200

    Element types

--- src/Elements.jl
+++ src/Elements.jl
@@ -1,13 +1,16 @@
 module Elements
 
+import Base: @_inline_meta
 using Base.Iterators: product
 using FastGaussQuadrature
 using StaticArrays
 
+using ..Transforms
 using ..Utils
 
 export ReferenceElement, Simplex, Tensor
 export quadrule
+export loctrans, globtrans
 
 
 """
@@ -64,6 +67,83 @@ function quadrule(self::Tensor{D}, npts::NTuple{D, Int}) where D
     rpts = (SVector(vcat(p...)) for p in product(pts...))
     (collect(SVector{D, Float64}, rpts), rwts)
 end
+
+
+abstract type AbstractElement{D} end
+
+Base.ndims(::Type{<:AbstractElement{D}}) where D = D
+Base.ndims(::AbstractElement{D}) where D = D
+
+
+"""
+    Element{D, I, T}
+
+A full D-dimensional element with index type I and transform type T.
+"""
+struct Element{D, I, Trf<:Transform}
+    index :: I
+    transform :: Trf
+end
+
+# Convenience constructor with a no-op index
+Element(trf) = Element(nothing, trf)
+
+@generated function Element(index, trf::Transform)
+    @assert fromdims(trf) == todims(trf)
+    quote
+        @_inline_meta
+        Element{$(todims(trf)), $index, $trf}(index, trf)
+    end
+end
+
+
+"""
+    SubElement{D, I, T, P}
+
+A D-dimensional element that is part of a higher-dimensional element
+of type P.
+"""
+struct SubElement{D, I, Trf<:Transform, Parent<:AbstractElement}
+    index :: I
+    transform :: Trf
+    parent :: Parent
+end
+
+# Convenience constructor with a no-op index
+SubElement(trf, parent) = SubElement(nothing, trf, parent)
+
+@generated function SubElement(index, trf::Transform, parent::Element)
+    @assert todims(trf) == ndims(parent)
+    quote
+        @_inline_meta
+        SubElement{$(fromdims(trf)), $index, $trf, $parent}(index, trf, parent)
+    end
+end
+
+
+"""
+    loctrans(::AbstractElement) :: Transform
+
+The transformation necessary to bring quadrature points into the fully
+realized parameter space of the master element. This is usually a
+no-op for Elements, and a chain of Updims for SubElements
+(i.e. boundary elements).
+"""
+function loctrans(::AbstractElement) end
+@inline loctrans(::Element{D}) where D = Empty{D,Float64}()
+@inline loctrans(self::SubElement) = Chain(self.transform, loctrans(self.parent))
+
+
+"""
+    globtrans(::AbstractElement) :: Transform
+
+The transformation necessary to bring a fully realized parameter space
+point into 'physical' space. This usually only depends on the master
+element, and is equal for all subelements.
+"""
+function globtrans(::AbstractElement) end
+@inline globtrans(self::Element) = self.transform
+@inline globtrans(self::SubElement) = globtrans(self.parent)
 
 
 end # module
